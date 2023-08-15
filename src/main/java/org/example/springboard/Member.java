b85a24d06cbc568cda0452efc809e3317ef7caf3
sllavv <129834569+choeseonmin@users.noreply.github.com>
2023-08-15 18:14:53 +0900

    first commit

---
+++ src/main/java/org/example/springboard/Member.java
@@ -0,0 +1,29 @@
+package org.example.springboard;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+//import org.springframework.data.annotation.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+}
